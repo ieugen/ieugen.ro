9a7fd148ebcabb0f715d1529caf84be026ca2848
Ioan Eugen Stan <eu@ieugen.ro>
2019-02-09 22:57:10 +0200

    Added the images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
+import './css/imports/cv.css';
+import './css/imports/master.css';
+import './css/imports/asciidoctor.css';
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
