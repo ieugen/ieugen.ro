eccaae415be37a59b21a6c88cf5ee276f823fea5
Eugen Stan <eugen.stan@netdava.com>
2020-12-22 20:15:28 +0200

    Fixed typo in fn name

--- src/cryogen/core.clj
+++ src/cryogen/core.clj
@@ -5,5 +5,5 @@
 
 (defn -main []
   (load-plugins)
-  (compile-assets-timed {:extend-params-fn custom/extended-params})
+  (compile-assets-timed {:extend-params-fn custom/extend-params})
   (System/exit 0))
