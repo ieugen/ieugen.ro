77f0c19084e04333152ef3049a534e553cde0240
Eugen Stan <eugen.stan@netdava.com>
2020-12-23 10:53:21 +0200

    Nimic important

--- src/cryogen/customization.clj
+++ src/cryogen/customization.clj
@@ -1,16 +1,16 @@
 (ns cryogen.customization)
 
-(defn extend-params [params other-pages]
+(defn extend-params [params {:keys [pages posts] :as other-pages}]
   (let [tag-count (->> (:posts-by-tag other-pages)
                        (map (fn [[k v]] [k (count v)]))
                        (into {}))
         tags-with-count (update
                          params :tags
                          #(map (fn [t] (assoc t :count (tag-count (:name t)))) %))
-        new-site-data (merge other-pages params tags-with-count)]
-    ; (spit "params.edn" (with-out-str (clojure.pprint/pprint params)))
-    ; (spit "site-data.edn" (with-out-str (clojure.pprint/pprint other-pages)))
-    ; (spit "merged.edn" (with-out-str (clojure.pprint/pprint new-site-data)))
+        new-site-data (assoc tags-with-count :posts posts :pages pages)]
+    ;; (spit "params.edn" (with-out-str (clojure.pprint/pprint params)))
+    ;; (spit "site-data.edn" (with-out-str (clojure.pprint/pprint other-pages)))
+    ;; (spit "merged.edn" (with-out-str (clojure.pprint/pprint new-site-data)))
     new-site-data))
 
 (comment
