2f9b310d4781f70534d41a969d69937c0f8e0663
Eugen Stan <eugen.stan@netdava.com>
2020-12-23 10:21:55 +0200

    Am rezolvat problema cu conținutul lipsă

--- src/cryogen/customization.clj
+++ src/cryogen/customization.clj
@@ -1,20 +1,22 @@
 (ns cryogen.customization)
 
-(defn extend-params [params site-data]
-  (let [tag-count (->> (:posts-by-tag site-data)
+(defn extend-params [params other-pages]
+  (let [tag-count (->> (:posts-by-tag other-pages)
                        (map (fn [[k v]] [k (count v)]))
-                       (into {}))]
-    ; (spit "params.txt" (prn-str site-data))
-    (->> (update
-          params :tags
-          #(map (fn [t] (assoc t :count (tag-count (:name t)))) %))
-         (merge site-data))))
-
+                       (into {}))
+        tags-with-count (update
+                         params :tags
+                         #(map (fn [t] (assoc t :count (tag-count (:name t)))) %))
+        new-site-data (merge other-pages params tags-with-count)]
+    ; (spit "params.edn" (with-out-str (clojure.pprint/pprint params)))
+    ; (spit "site-data.edn" (with-out-str (clojure.pprint/pprint other-pages)))
+    ; (spit "merged.edn" (with-out-str (clojure.pprint/pprint new-site-data)))
+    new-site-data))
 
 (comment
 
-  (let [params {:a 1 :b 2}
-        site-data {:x 3 :y 4}]
+  (let [params {:a 1 :b 2 :c {:a 33 :b 55}}
+        site-data {:x 3 :y 4 :c {:z 99}}]
     (->> (update params :tags (fn [_] {:t 1 :z 2}))
          (merge site-data)))
 
