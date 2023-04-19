92e595afb88981e8d0536439ae3bf4d531647240
Eugen Stan <eugen.stan@netdava.com>
2023-04-20 01:19:58 +0300

    Using asciidoctor and asciidoctor-pdf cli tools

--- project.clj
+++ project.clj
@@ -6,17 +6,12 @@
             :dependencies [[org.clojure/clojure "1.10.1"]
                            [ring/ring-devel "1.8.2"]
                            [compojure "1.6.2"]
-                           [http-kit "2.5.0"]
                            [ring-server "0.5.0"]
-                           [cryogen-asciidoc "0.3.3"]
-                           [cryogen-core "0.4.0"]
-                           [clj-livereload "0.2.0"]
-                           [org.slf4j/slf4j-simple "1.7.30"]
-                           ]
+                           [cryogen-flexmark "0.1.4"]
+                           [cryogen-core "0.4.0"]]
             :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
-                   :handler cryogen.server/handler})
-
-
-
+                   :handler cryogen.server/handler}
+            :aliases {"serve"      ["run" "-m" "cryogen.server"]
+                      "serve:fast" ["run" "-m" "cryogen.server" "fast"]})
