c171d1a9f17ad667c09050c7756eb9536e9bb416
Eugen Stan <eugen.stan@netdava.com>
2023-04-20 01:37:52 +0300

    Am renunțat la proiect gradle și la cv.txt

--- src/cryogen/core.clj
+++ src/cryogen/core.clj
@@ -1,24 +1,8 @@
 (ns cryogen.core
   (:require [cryogen-core.compiler :refer [compile-assets-timed]]
-            [cryogen-core.plugins :refer [load-plugins]]
-            [cryogen.customization :as c]))
+            [cryogen-core.plugins :refer [load-plugins]]))
 
 (defn -main []
   (load-plugins)
-  (compile-assets-timed {:extend-params-fn c/extend-params})
+  (compile-assets-timed)
   (System/exit 0))
-
-
-(comment
-
-  ;; This does not print hellprrr
-  ;; (compile-assets-timed {:extend-params-fn extend-params})
-
-  ;; This does print hellp
-  (compile-assets-timed {:extend-params-fn  (fn extend-params [params site-data]
-                                              (println "hellp")
-                                              params)})
-
-  (-main)
-
-  0)
